dbaf0ff408caa1b58fe57bd9804fbc65065da782
Sebastien MANICON <SManicon@free.fr>
2017-04-25 21:55:27 +0200

    Parser - Refacto TU InstructionExecutor
    Factorisation du code de declaration duplique dans une methode setUp.

--- src/test/java/org/smanicon/mowitnow/parser/InstructionExecutorTest.java
+++ src/test/java/org/smanicon/mowitnow/parser/InstructionExecutorTest.java
@@ -1,21 +1,33 @@
 package org.smanicon.mowitnow.parser;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InOrder;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.smanicon.mowitnow.models.LawnMower;
 
 import java.util.Arrays;
 
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.smanicon.mowitnow.parser.InstructionSet.*;
 
 public class InstructionExecutorTest {
+
+    @Mock
+    private LawnMower lawnMower;
+
+    private InstructionExecutor instructionExecutor;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        instructionExecutor = new InstructionExecutor(lawnMower);
+    }
+
     @Test
     public void should_execute_instruction_right_turn_lawnmower() {
-        LawnMower lawnMower = mock(LawnMower.class);
-        InstructionExecutor instructionExecutor = new InstructionExecutor(lawnMower);
         instructionExecutor.execute(Arrays.asList(D));
 
         verify(lawnMower).turnRight();
@@ -23,8 +35,6 @@ public class InstructionExecutorTest {
 
     @Test
     public void should_execute_instruction_left_turn_lawnmower() {
-        LawnMower lawnMower = mock(LawnMower.class);
-        InstructionExecutor instructionExecutor = new InstructionExecutor(lawnMower);
         instructionExecutor.execute(Arrays.asList(G));
 
         verify(lawnMower).turnLeft();
@@ -32,8 +42,6 @@ public class InstructionExecutorTest {
 
     @Test
     public void should_execute_instruction_move_lawnmower() {
-        LawnMower lawnMower = mock(LawnMower.class);
-        InstructionExecutor instructionExecutor = new InstructionExecutor(lawnMower);
         instructionExecutor.execute(Arrays.asList(A));
 
         verify(lawnMower).move();
@@ -41,8 +49,6 @@ public class InstructionExecutorTest {
 
     @Test
     public void should_execute_many_instruction_on_lawnmower() {
-        LawnMower lawnMower = mock(LawnMower.class);
-        InstructionExecutor instructionExecutor = new InstructionExecutor(lawnMower);
         instructionExecutor.execute(Arrays.asList(A, D, A, G));
 
         InOrder inOrder = Mockito.inOrder(lawnMower);
